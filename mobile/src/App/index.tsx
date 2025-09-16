3fcf5e483505507e3ecaaa15dc50444672469c73
vyang <v.yang2968@gmail.com>
2025-09-16 19:17:29 -0400

    add event provider to app root

--- mobile/src/App/index.tsx
+++ mobile/src/App/index.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import TabNavigator from './TabNavigator';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import TabNavigator from "./TabNavigator";
+import { EventProvider } from "react-native-outside-press";
 
-export { default as TabNavigator } from './TabNavigator';
+export { default as TabNavigator } from "./TabNavigator";
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <TabNavigator />
+        <EventProvider>
+          <TabNavigator />
+        </EventProvider>
       </NavigationContainer>
     </SafeAreaProvider>
   );
